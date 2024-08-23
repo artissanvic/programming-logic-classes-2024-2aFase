c6261ebd6044f0e6ac3b41ea53862768b86e25f6
artissanvic <hillohinks@gmail.com>
2024-08-23 17:16:14 -0300

    Criação do primeiro projeto com react bonitão

---
+++ OiReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <h1>Dinossauros</h1>
+   <h2>Carnívorios</h2>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi minima laborum a quidem, itaque id. Cum, consequatur voluptate velit beatae recusandae quaerat alias explicabo ab, nihil, magni optio corporis fugit?</p>
+
+    </>
+  )
+}
+
+export default App
