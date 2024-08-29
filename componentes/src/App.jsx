ed08464ba1a8ceecc46242363960ebda475d617f
artissanvic <hillohinks@gmail.com>
2024-08-29 17:20:59 -0300

    add criação de componentes e mais umas coisas legais e gostosinhas

---
+++ componentes/src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+
+  <h1>Atalhos do vs code</h1>
+  <h2>coisas legais para facilitar sua vida</h2>
+
+<p> Comando para o emmet da ul: ul>li*6 </p>
+
+  <ul>
+    <li>atalho: ALT + Z para quebra automatica de linha </li>
+    <li>atalho: ALT + click esquerdo para adicionar novos cursores na tela </li>
+    <li>atalho: CTRL + SHIFT + K para apagar uma linha inteira </li>
+    <li>atalho: CTRL + ; para comentar uma linha </li>
+    <li>atalho: ALT + setinhas⬆⬇ para mover a linha </li>
+    <li>atalho: ALT+SHIFT+setinha copia a linha atual ou bloco selecionados </li>
+    <li>atalho: CTRL + J ou CTRL + ' abre/fecha o terminal </li>
+
+  </ul>
+
+  <p>para esfregar varios cursores pela tela (digitar varias coisas ao mesmo tempo) clique com a rodinha e esfrega ela pela tela(arraste)</p>
+  <p>OU pressione o ALT e vá clicando no que quer modificar</p>
+
+  <p>Extensoes</p>
+  <ul>
+    <li>ES7 React/Redux/GraphQL...</li>
+    <li>Auto Rename Tag</li>
+    <li>Color Highlight</li>
+    <li>power mode</li>
+    <li>Vscode pets</li>
+  </ul>
+  </>
+  )
+}
+
+export default App
---
+++ componentesDeVerdade/src/components/Links.jsx
@@ -0,0 +1,9 @@
+function Links() {
+  return (
+    <div>
+      <p>Links | link2 | link3 (tudo de verdade)</p>
+    </div>
+  )
+}
+
+export default Links
