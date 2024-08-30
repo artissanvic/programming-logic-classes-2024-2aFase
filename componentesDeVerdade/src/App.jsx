0b9751bff821209bb5627f3d8c19e18b4abae3d1
artissanvic <hillohinks@gmail.com>
2024-08-30 17:06:33 -0300

    ajeitamos a tela com cores e imagens novas, criamos o corpo a pagina

--- componentesDeVerdade/src/App.jsx
+++ componentesDeVerdade/src/App.jsx
@@ -1,14 +1,20 @@
 import './App.css'
 import Cabecalho from './components/Cabecalho'
+import Corpo from './components/Corpo'
 import Links from './components/Links'
 
 function App() {
 
   return (
+    <center>
+
     <>
      <Cabecalho/>
      <Links/>
+     <Corpo/>
     </>
+    
+    </center>
   )
 }
 
---
+++ componentesDeVerdade/src/components/Corpo.jsx
@@ -0,0 +1,12 @@
+function Corpo() {
+  return (
+    <div>
+        <h2>Eu sou o corpinho da página. Sou muito mais que um corpinho bonito....</h2>
+        <img src="./Imagens/guaxinim fofo.jfif" alt="" />
+        <img src="./Imagens/raposa.png" alt="" />
+      
+    </div>
+  )
+}
+
+export default Corpo
